f90e791d6cbe4c8e2940d2ef5b7f5ccc3c50f01a
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2024-11-06 01:34:49 +0300

    Task-2 finished

---
+++ task-2.js
@@ -0,0 +1,10 @@
+function getShippingMessage(country, price, deliveryFee){
+        const x=`Shipping to ${country} will cost ${price + deliveryFee}`;
+        return x;
+}
+
+getShippingMessage();
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
