8d07a92e686defdbfefffb4c848b9a7e0651ad73
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2024-11-06 15:16:30 +0300

    Task3 changed

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function getElementWidth(content, padding, border){
-    const write=`${Number.parseFloat(content) + 2*Number.parseFloat(padding) + 2*Number.parseFloat(border)}px`;
-    return write;
+    const totalWidth=Number.parseFloat(content) + 2*Number.parseFloat(padding) + 2*Number.parseFloat(border);
+    return totalWidth;
 }
 
 getElementWidth()
@@ -8,4 +8,3 @@ getElementWidth()
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
-
